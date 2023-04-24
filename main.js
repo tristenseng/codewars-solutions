b36a3cbfd1d449d5dbfd911779b4fafc7890a0dc
Tristen <tristenseng@gmail.com>
2023-04-24 09:02:20 -0700

    adding comments, adding solution

--- main.js
+++ main.js
@@ -2,10 +2,45 @@
 KYU RANK - CHALLENGE NAME
 */
 
+
+
+
+
+
+
+
+
+
+/*
+8 - Beginner Series #4 Cockroach
+*/
+//argument takes in a km per hour speed
+//return speed in cm per second
+function cockroachSpeed(s) {
+
+  //   km/h * h/3600s * 100000cm/km = km/s
+  //   why 250/9? work out the math then simplify
+  return Math.floor(s*(250/9))
+}
+//returns 30
+console.log(1.08)
+
+
+
+
+
+
+
+
+
+
 /*
 6 - Find the odd int
 */
-
+//argument takes in an array of integers
+//only one of those integers will appear an odd number of times within
+//the array
+//return that odd number
 function findOdd(A) {
 
   //iterate through A array to add keys : values to counter dictionary
@@ -21,7 +56,6 @@ function findOdd(A) {
     }
   }
 
-
   //iterate through counter and find the first odd value
   //return the odd value
   for (let key of Object.keys(counter)) {
@@ -33,26 +67,34 @@ function findOdd(A) {
 console.log(findOdd([1,2,2,3,3,3,4,3,3,3,2,2,1]))
 
 
+
+
+
+
+
+
+
+
 /*
 6 - Sort the odd
 */
+//array takes in an array of numbers
+//return the odd numbers sorted in ascending order
+//leave the even numbers where they are
 function sortArray(arr) {
   let oddIndexes = []
   //indexes refering to only the odd number indexes
   //learn the modulus of negative numbers
   let oddArr = arr.filter((num, i) => (num % 2 == 1 || num % 2 == -1) && oddIndexes.push(i))
 
-
   //sort the odd numbers
   //learned arr.sort() doesnt automatically sort in ascending order
   oddArr = oddArr.sort((a,b) => a-b)
-
 
   //delete odd number indexes from arr
   for (i of oddIndexes) {
     delete arr[i]
   }
-
 
   //place odd numbers back into the empty index
   for (let i = 0; i < oddIndexes.length; i++) {
@@ -60,9 +102,11 @@ function sortArray(arr) {
   }
   return arr
 }
-
 // let ans = sortArray([1,2,3,4,5,6,7,-1,-2,-3,-4,-5,-6,-7])
 // console.log(ans)
+
+
+
 
 
 
@@ -73,7 +117,12 @@ function sortArray(arr) {
 /*
 6 - Consecutive strings
 */
+//starr is an array of strings
+//k is the amount of times we would like to concatenate
+//strings consecutive to one another
+//return the longest concatenated string given the k constraint
 function longestConsec(strarr, k) {
+    //take care of edge cases/simple cases
     if (strarr == "null" || strarr.length == 0 || k <= 0 || k > strarr.length) {
       return ""
     }
@@ -83,24 +132,15 @@ function longestConsec(strarr, k) {
     else if (strarr.length == k-1) {
         return strarr.join("")
     }
+
+    //push concatenated strings onto longestStrArr
     let longestStrArr = []
     for (let i = 0; i <= strarr.length - k; i++) {
         longestStrArr.push(strarr.slice(i,i+k).join(""))
     }
+
+    //make an array sorted by string length (longest string first)
+    //return the 0th element in the array (the longest string)
     return longestStrArr.sort((a,b) => b.length - a.length)[0]
-    //return longest element in the array
-/*
-k = 4
-first iter: 0: 6-4= 2
-slice 0, 4
-
-second iter: 1: 2
-slice 1, 5
-
-third iter: 2: 2
-slice 2, 6
-*/
-    
-  
 }
 //console.log(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], 2))
