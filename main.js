811e15f8ad4df01851fd40e4d743a93e1914b438
Tristen <tristenseng@gmail.com>
2023-04-29 18:50:54 -0700

    adding solution

--- main.js
+++ main.js
@@ -4,6 +4,39 @@ KYU RANK - CHALLENGE NAME
 
 
 /*
+6 - Multiples of 3 or 5
+*/
+//If we list all the natural numbers below 10 that are multiples of 3 or 5, 
+//we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//Finish the solution so that it returns the sum of all the multiples 
+//of 3 or 5 below the number passed in. 
+//Additionally, if the number is negative, return 0
+function solution(n){
+  //input validation
+  if (n < 1) {
+    return 0
+  }
+  else {
+    //for loop that iterates to n
+    //adds any multiple of 3 or 5 to the set
+    let mults = new Set();
+    for (let i = 1; i < n; i++) {
+      if (i % 3 == 0 || i % 5 == 0) {
+        mults.add(i)
+      }
+    }
+    //return sum of all values within the set
+    return [...mults].reduce((iter, val) => val + iter, 0)
+  }
+  
+}
+console.log(solution(10))
+
+
+
+
+
+/*
 7 - Sum of odd numbers
 */
 //given a triangle of consecutive odd numbers
