d775891650fd6045cdc74cf88cdfd5f2edb7d546
Tristen <tristenseng@gmail.com>
2023-08-15 06:24:41 -0700

    adding solution

--- leet.js
+++ leet.js
@@ -3,7 +3,25 @@ DIFFICULTY - CHALLENGE NAME
 */
 
 
+/**
+ * Easy - Two Sum
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  let result = []
+  for (let i = 0; i < nums.length-1;i++) {
+      for (let j = 1; j < nums.length; j++) {
+          if (nums[i] + nums[j] == target && i != j) {
+              result.push(i)
+              result.push(j)
+              return result
+          }
 
+      }
+  }
+};
 
 
 /*
