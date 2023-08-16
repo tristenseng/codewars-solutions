25b72df67f15757135a36ec5594f674e1a057ffa
Tristen <tristenseng@gmail.com>
2023-08-16 06:47:01 -0700

    adding solution

--- leet.js
+++ leet.js
@@ -2,6 +2,19 @@
 DIFFICULTY - CHALLENGE NAME
 */
 
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+  x = x.toString()
+  let temp = []
+  for (let i = x.length-1; i >= 0; i--) {
+      temp.push(x[i])
+  } 
+
+  return x == temp.join("")
+};
 
 /**
  * Easy - Two Sum
